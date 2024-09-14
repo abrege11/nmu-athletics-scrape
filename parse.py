3974ef26e04eb6eff91a8fadc9c880d761baf254
abe brege <bregeabe@gmail.com>
2024-09-14 14:32:36 -0400

    overall win/loss totals for teams parsed

--- parse.py
+++ parse.py
@@ -1,8 +1,20 @@
 import sqlite3
+from collections import defaultdict
 
 connection = sqlite3.connect('sports.db')
 db = connection.cursor()
-
+length = len(["wBasketball", "wLacrosse", "wSoccer", "wVolleyBall", "wWrestling", "mBasketball", "mFootball", "mHockey", "mSoccer"])
+namingDictionary = {
+    1: "Womens Basketball",
+    2: "Womens Lacrosse",
+    3: "Womens Soccer",
+    4: "Womens Volleyball",
+    5: "Womens Wrestling",
+    6: "Mens Basketball",
+    7: "Mens Football",
+    8: "Mens Hockey",
+    9: "Mens Soccer"
+}
 data = db.execute("SELECT ovrValue, sportsId, year FROM sportsInfo").fetchall()
 sportsNames = db.execute("SELECT * FROM sports").fetchall()
 
@@ -32,10 +44,30 @@ def getAllValues():
         values.append(scorecard.printValues())
     return values
 
-values = getAllValues()
-print(values)
+def getTotals():
+    values = getAllValues()
+    res = defaultdict(list)
+    temp = []
+    for value in values:
+        res[value[3]].append(value)
+    for key, val in res.items():
+        temp = [0, 0, 0, 0, []]
+        for i in range(len(val)-1):
+            temp[0] += int(val[i][0])
+            temp[1] += int(val[i][1])
+            temp[2] += int(val[i][2])
+            temp[3] = key
+            temp[4].append(val[i][4])
+        
+        res[key] = temp[:]
+        temp.clear()
+    return res
 
-
+# values = getAllValues()
+# print(values)
+vals = getTotals()
+for val in vals.values():
+    print(f"{namingDictionary[val[3]]}: {val}\n")
 
 
 connection.commit()
